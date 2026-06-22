13c920fc909a28a964059223205a33920827df0b
LAPTOP-B3SC8PH8\Lydia George Michael <georgelydia42@gmail.com>
2026-06-22 20:56:30 +0300

    create mock_products.dart and add its asset images

--- lib/core/constants/image_strings.dart
+++ lib/core/constants/image_strings.dart
@@ -35,4 +35,29 @@ class DImageStrings {
   static const String skinCare= 'lib/assets/images/jpg/categories_imgs/Skin Care.jpg';
 
 
+  /// -- PRODUCTS IMAGES
+  static const String paracetamol500mg = 'lib/assets/images/jpg/products/Paracetamol 500mg.jpeg';
+  static const String voltarenGel = 'lib/assets/images/jpg/products/Voltaren Gel.jpg';
+  static const String amoxicillin250mg = 'lib/assets/images/jpg/products/Amoxicillin 250mg.jpeg';
+  static const String azithromycin500mg ='lib/assets/images/jpg/products/Azithromycin 500mg.jpeg';
+  static const String vitaminC1000mg ='lib/assets/images/jpg/products/Vitamin C 1000mg.jpeg';
+  static const String vitaminD35000IU ='lib/assets/images/jpg/products/Vitamin D3 5000IU.jpeg';
+  static const String ibuprofen400mg = 'lib/assets/images/jpg/products/Ibuprofen 400mg.jpeg';
+  static const String ketofanGel = 'lib/assets/images/jpg/products/ketofanGel.jpeg';
+  static const String cefixime = 'lib/assets/images/jpg/products/cefixime.jpeg';
+  static const String augmentin = 'lib/assets/images/jpg/products/augmentin1g.jpeg';
+  static const String omega3 = 'lib/assets/images/jpg/products/omega3.jpeg';
+  static const String multivitamin = 'lib/assets/images/jpg/products/multivitamin.jpeg';
+  static const String ventolinInhaler = 'lib/assets/images/jpg/products/Ventolin Inhaler.jpeg';
+  static const String otrivinNasalSpray = 'lib/assets/images/jpg/products/Otrivin Nasal Spray.jpeg';
+  static const String mucosolSyrup = 'lib/assets/images/jpg/products/mucosolSyrup.jpeg';
+  static const String strepsils = 'lib/assets/images/jpg/products/strepsils.jpeg';
+  static const String panthenolHairCream = 'lib/assets/images/jpg/products/panthenolHairCream.jpeg';
+  static const String vichyDercosShampoo = 'lib/assets/images/jpg/products/vichyDercosShampoo.jpeg';
+  static const String ceraveMoisturizingCream = 'lib/assets/images/jpg/products/ceraveMoisturizingCream.jpeg';
+  static const String arganHairSerum = 'lib/assets/images/jpg/products/arganHairSerum.jpeg';
+  static const String panthenolCream = 'lib/assets/images/jpg/products/panthenolCream.jpeg';
+  static const String laRochePosayCleanser = 'lib/assets/images/jpg/products/laRochePosayCleanser.jpeg';
+  static const String neutrogenaHydroBoost = 'lib/assets/images/jpg/products/neutrogenaHydroBoost.jpeg';
+
 }
