e5f0e2abbf1c233086f3c288ed3713760e04a539
LAPTOP-B3SC8PH8\Lydia George Michael <georgelydia42@gmail.com>
2026-06-23 11:49:25 +0300

    handle auth_repo.dart

--- lib/data/repositories/auth_repo.dart
+++ lib/data/repositories/auth_repo.dart
@@ -1,7 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthRepository {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<UserCredential> login(String email, String password) async {
     return await _auth.signInWithEmailAndPassword(
@@ -11,20 +13,30 @@ class AuthRepository {
   }
 
   Future<UserCredential> register(String email, String password, String fullName) async {
-    return await _auth.createUserWithEmailAndPassword(
+
+    final credential = await _auth.createUserWithEmailAndPassword(
       email: email,
       password: password,
     );
+    await _firestore.collection('users').doc(credential.user!.uid).set({
+      'fullName' : fullName,
+      'email': email,
+      'createdAt' : FieldValue.serverTimestamp(),
+    });
 
-
+    return credential;
   }
 
+
+
+
+
   Future<void> resetPassword (String email) async {
-    return await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+     await _auth.sendPasswordResetEmail(email: email);
   }
 
   Future<void> logout() async{
-    await FirebaseAuth.instance.signOut();
+    await _auth.signOut();
   }
 
 }
