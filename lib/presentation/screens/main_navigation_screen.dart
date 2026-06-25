757deb87b8f1596907a8d3b16c1cc0787c1bbf65
LAPTOP-B3SC8PH8\Lydia George Michael <georgelydia42@gmail.com>
2026-06-25 16:20:36 +0300

    create main_navigation_screen.dart

---
+++ lib/presentation/screens/main_navigation_screen.dart
@@ -0,0 +1,67 @@
+import 'package:dawaya/core/constants/app_colors.dart';
+import 'package:dawaya/presentation/cubits/cart/cart_cubit.dart';
+import 'package:dawaya/presentation/screens/cart/cart_screen.dart';
+import 'package:dawaya/presentation/screens/home/home_screen.dart';
+import 'package:dawaya/presentation/screens/profile/profile_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MainNavigationScreen extends StatefulWidget {
+  const MainNavigationScreen({super.key});
+
+  @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _screens = const [
+    HomeScreen(),
+    CartScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(index: _currentIndex, children: _screens),
+
+      bottomNavigationBar: BlocBuilder<CartCubit, CartState>(
+        builder: (context, state) {
+          return BottomNavigationBar(
+            currentIndex: _currentIndex,
+            onTap: (index) {
+              setState(() => _currentIndex = index);
+            },
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: DColors.primaryColorPest,
+            unselectedItemColor: DColors.primaryColorBlue,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined),
+                activeIcon: Icon(Icons.home),
+                label: 'Home',
+              ),
+
+              BottomNavigationBarItem(
+                icon: Badge(
+                  label: Text('${state.items.length}'),
+                  isLabelVisible: state.items.isNotEmpty,
+                  child: Icon(Icons.add_shopping_cart),
+                ),
+                label: 'Cart',
+              ),
+
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.person),
+                activeIcon: Icon(Icons.person),
+                label: 'Profile'
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
