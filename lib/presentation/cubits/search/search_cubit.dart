c9406bba1baab4298dab9824e7129fcbb6bd4e55
LAPTOP-B3SC8PH8\Lydia George Michael <georgelydia42@gmail.com>
2026-06-25 12:20:02 +0300

    create search_state.dart create search_cubit.dart

--- lib/presentation/cubits/search/search_cubit.dart
+++ lib/presentation/cubits/search/search_cubit.dart
@@ -1,8 +1,35 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:dawaya/data/models/pharmacy/pharmacy_model.dart';
+import 'package:dawaya/data/models/search/search_result_model.dart';
+import 'package:dawaya/data/service/api_service/search/search_repo.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'search_state.dart';
 
 class SearchCubit extends Cubit<SearchState> {
-  SearchCubit() : super(SearchInitial());
+  final SearchRepo _repo;
+
+  SearchCubit(this._repo) : super(SearchState());
+
+  Future<void> search(String query, List<PharmacyModel> pharmacies) async {
+    if (query.trim().isEmpty) {
+      emit(const SearchState());
+      return;
+    }
+
+    emit(state.copyWith(isLoading: true, hasSearched: true));
+    try {
+      final results = await _repo.searchAcrossPharmacies(
+        query: query,
+        pharmacies: pharmacies,
+      );
+      emit(state.copyWith(results: results));
+    } catch (e) {
+      emit(state.copyWith(errorMessage: 'Search failed. Try again'));
+    }
+  }
+
+  void clearSearch(){
+    emit(SearchState());
+  }
+
 }
--- lib/presentation/cubits/search/search_state.dart
+++ lib/presentation/cubits/search/search_state.dart
@@ -1,6 +1,29 @@
 part of 'search_cubit.dart';
 
-@immutable
-sealed class SearchState {}
+class SearchState {
+  final bool isLoading;
+  final List<SearchResultModel> results;
+  final String? errorMessage;
+  final bool hasSearched;
 
-final class SearchInitial extends SearchState {}
+  const SearchState({
+    this.isLoading = false,
+    this.results = const [],
+    this.errorMessage,
+    this.hasSearched = false,
+  });
+
+  SearchState copyWith({
+    bool? isLoading,
+    List<SearchResultModel>? results,
+    String? errorMessage,
+    bool? hasSearched,
+  }) {
+    return SearchState(
+      isLoading: isLoading ?? false,
+      results: results ?? this.results,
+      errorMessage: errorMessage,
+      hasSearched: hasSearched ?? this.hasSearched,
+    );
+  }
+}
